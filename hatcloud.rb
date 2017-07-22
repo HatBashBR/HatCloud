443b6ea23584a479e0a9ab0c5baccadca05649c4
Mateus Lino <0daycode@protonmail.com>
2017-07-22 15:57:45 -0300

    Update hatcloud.rb

--- hatcloud.rb
+++ hatcloud.rb
@@ -12,12 +12,15 @@ green = "\033[01;32m"
 
 
 puts "\n"
-puts" ██████╗██╗      ██████╗ ██╗   ██╗██████╗     ██╗  ██╗ █████╗ ████████╗ "
-puts"██╔════╝██║     ██╔═══██╗██║   ██║██╔══██╗    ██║  ██║██╔══██╗╚══██╔══╝ "
-puts"██║     ██║     ██║   ██║██║   ██║██║  ██║    ███████║███████║   ██║   "
-puts"██║     ██║     ██║   ██║██║   ██║██║  ██║    ██╔══██║██╔══██║   ██║   "
-puts"╚██████╗███████╗╚██████╔╝╚██████╔╝██████╔╝    ██║  ██║██║  ██║   ██║   "
-puts" ╚═════╝╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   "
+puts"██╗  ██╗ █████╗ ████████╗     ██████╗██╗      ██████╗ ██╗   ██╗██████╗ "
+puts"██║  ██║██╔══██╗╚══██╔══╝    ██╔════╝██║     ██╔═══██╗██║   ██║██╔══██╗"
+puts"███████║███████║   ██║       ██║     ██║     ██║   ██║██║   ██║██║  ██║"
+puts"██╔══██║██╔══██║   ██║       ██║     ██║     ██║   ██║██║   ██║██║  ██║"
+puts"██║  ██║██║  ██║   ██║       ╚██████╗███████╗╚██████╔╝╚██████╔╝██████╔╝"
+puts"╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═════╝╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝ "
+                                                                       
+
+
 puts "#{red}Tool for identifying real IP of CloudFlare protected website." 
 puts "#{green}\x41\x75\x74\x68\x6f\x72\x3a\x20\x4d\x61\x74\x65\x75\x73\x20\x61\x2e\x6b\x2e\x61\x20\x44\x63\x74\x6f\x72\x2e\x20\x2d\x20\x48\x61\x74\x42\x61\x73\x68\x20\x42\x52"
 puts "#{green}\x4d\x65\x6d\x62\x65\x72\x73\x20\x48\x61\x74\x42\x61\x73\x68\x42\x52\x3a\x20\x45\x76\x65\x72\x74\x6f\x6e\x20\x61\x2e\x6b\x2e\x61\x20\x20\x58\x47\x55\x34\x52\x44\x31\x34\x4e\x2c\x20\x4a\x75\x6e\x69\x6f\x72\x20\x61\x2e\x6b\x2e\x61\x20\x41\x53\x54\x41\x52\x4f\x54\x48\x20\x2c\x20\x55\x72\x64\x53\x79\x73\x20\x61\x2e\x6b\x2e\x61\x20\x4a\x6f\x68\x6e\x6e\x79\x2c\x20\x4e\x6f\x20\x6f\x6e\x65\x2c\x20\x47\x65\x6f\x76\x61\x6e\x65\x2c\x20\x52\x48\x6f\x6f\x64"
@@ -56,18 +59,18 @@ banner()
 if options[:bypass].nil?
     puts "Insert URL -b or --byp"
 else
-l = options[:bypass]
-uri = URI ("http://www.crimeflare.com/cgi-bin/cfsearch.cgi")
-    res = Net::HTTP.post_form(uri, 'cfS' => options[:bypass])
+option = options[:bypass]
+payload = URI ("http://www.crimeflare.com/cgi-bin/cfsearch.cgi")
+    request = Net::HTTP.post_form(payload, 'cfS' => options[:bypass])
         
-        x =  res.body 
-y = /(\d*\.\d*\.\d*\.\d*)/.match(x)
-    k = IPSocket.getaddress (options[:bypass])
+        response =  request.body 
+regex = /(\d*\.\d*\.\d*\.\d*)/.match(response)
+    ip_real = IPSocket.getaddress (options[:bypass])
     
-    puts "[+] Site analysis: #{l} "
-    puts "[+] CloudFlare IP is #{k} "
-    puts "[+] Real IP is #{y}"
-        target = "http://ipinfo.io/#{y}/json"
+    puts "[+] Site analysis: #{option} "
+    puts "[+] CloudFlare IP is #{ip_real} "
+    puts "[+] Real IP is #{regex}"
+        target = "http://ipinfo.io/#{regex}/json"
         url = URI(target).read
         json = JSON.parse(url)
     puts "[+] Hostname: " + json['hostname']
